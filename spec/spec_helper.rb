7311e8a5405ac0083bbd59ac122253aaf2c2e59f
pyama86 <pyama@pepabo.com>
2019-12-19 11:02:02 +0900

    add spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,12 @@ require 'wazuh'
 require 'webmock/rspec'
 
 Wazuh.configure do |config|
+  config.api_endpoint = "https://localhost:55000"
+end
+
+def stub_api(method, path)
+  url = "https://localhost:55000#{path}"
+  file = "spec/fixtures#{path}/#{method}.json"
+  file << '_list' if File.directory?(file)
+  stub_request(method, url).to_return(status: 200, body: File.read(file))
 end
--- spec/wazuh_spec.rb
+++ spec/wazuh_spec.rb
@@ -4,4 +4,56 @@ describe Wazuh do
   it 'has a version number' do
     expect(Wazuh::VERSION).not_to be nil
   end
+
+  describe '.#respond_to' do
+    subject { described_class }
+
+    it { is_expected.to be_respond_to(:agents) }
+    it { is_expected.to be_respond_to(:delete_agents) }
+  end
+
+  shared_examples_for 'a agent' do
+    it do
+      expect(agent).to           be_a Hash
+    end
+  end
+
+  describe '.#agents' do
+    before do
+      stub_api(:get, '/agents')
+    end
+
+    subject { agents }
+
+    let!(:agents) do
+      described_class.agents
+    end
+
+    let(:agent) { agents["items"][0] }
+
+    it do
+      is_expected.to be_a Hash
+    end
+
+    it_should_behave_like 'a agent'
+  end
+
+  describe '.#delete_agents' do
+    before do
+      stub_api(:delete, '/agents')
+    end
+
+    subject { total_affected_agents }
+
+    let!(:response) do
+      described_class.delete_agents
+    end
+
+    let(:total_affected_agents) { response["total_affected_agents"] }
+
+    it do
+      is_expected.to be 2 
+    end
+  end
+
 end
